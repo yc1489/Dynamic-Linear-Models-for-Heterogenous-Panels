e24ea78593fdff882330f6693452bcb8439f8413
yc1489 <henry0929henry0929@gmail.com>
2019-10-17 18:55:55 +0100

    up

--- MC_code/LS.m
+++ MC_code/LS.m
@@ -1,19 +1,28 @@
-rep = 1000;  
+rep = 2000;  
 list_T = [25 50 100 200]; 
 list_N = [25 50 100 200];  
-list_phi= [0.25]; 
+list_phi= [0.5]; 
 rho=[0.5];
-k=1;   % number of regressors
+b1=3;
+b2=1;
+rho_b=0.4;
+b=[b1,b2];
+k=2;   % number of regressors
 bias_mean_phi=zeros(size(list_T,2), size(list_N,2), size(list_phi,2)); 
-bias_mean_beta=zeros(size(list_T,2), size(list_N,2)); 
+bias_mean_beta1=zeros(size(list_T,2), size(list_N,2)); 
+bias_mean_beta2=zeros(size(list_T,2), size(list_N,2)); 
 std_phi=zeros(size(list_T,2), size(list_N,2), size(list_phi,2));  
-std_beta=zeros(size(list_T,2), size(list_N,2));  
+std_beta1=zeros(size(list_T,2), size(list_N,2));  
+std_beta2=zeros(size(list_T,2), size(list_N,2));  
 rmse_phi=zeros(size(list_T,2), size(list_N,2), size(list_phi,2));
-rmse_beta=zeros(size(list_T,2), size(list_N,2));
+rmse_beta1=zeros(size(list_T,2), size(list_N,2));
+rmse_beta2=zeros(size(list_T,2), size(list_N,2));
+
 
 for idx_phi=1:size(list_phi,2)     
 phi= list_phi(idx_phi);  
-  b=1-phi; 
+  b1=3;
+  b2=1;
 for idx_T=1:size(list_T,2)    
 T0 = list_T(idx_T);             
  
@@ -23,68 +32,93 @@ Dis_T=50;
 
 TT= (T0+1)+Dis_T;    
 
-
 LS_MG=zeros(1+k,rep);   % 1+k by rep
 sml=1;         
-%while sml<=rep
-parfor sml=1:rep
+while sml<=rep
+%parfor sml=1:rep
 y=zeros(N,TT);
-x=zeros(N,TT);
-%phi_i= phi+  (0.8)*rand([1,N]);
-phi_i= phi*ones(1,N);
-beta_i=b+0.5+(0.5)*rand([1,N]); 
+x=zeros(k,N,TT);
+
+eta_rho_i=-0.25+(0.5)*rand([1,N]); % 1 by N
+phi_i= phi+  eta_rho_i;
+
+beta_i=b'*ones(1,N)+sqrt(1-rho_b^(2))*ones(k,1)*eta_rho_i; % k by N
+
 theta_i=[phi_i;beta_i];   % 1+k by N
+v_x=zeros(k,N,TT);
+v_x(:,:,1)=zeros(k,N,1);  
+for ttt=2:TT
+    for iiii=1:N
+v_x(:,iiii,ttt)=0.5*v_x(:,iiii,ttt-1)+sqrt((1-0.5^(2)))*(0.5+rand(k,1)); % k by N by TT
+    end 
+end
+
+
 
 
 y(:,1)=zeros(N,1);  
-x(:,1)=zeros(N,1);  
+x(:,:,1)=zeros(k,N,1); 
 for tt=2:TT
     for ii=1:N
-   x(ii,tt)=rho*(x(ii,tt-1))+normrnd(0,1);     % N by TT
- %x(ii,tt)=normrnd(0,1);     % N by TT
-  y(ii,tt)= ([y(ii,tt-1),x(ii,tt)])*theta_i(:,ii)+normrnd(0,1);    % N by TT
+ x(:,ii,tt)=rho*(x(:,ii,tt-1))+v_x(:,ii,tt);     % k by N by TT
+ 
+  y(ii,tt)= ([y(ii,tt-1),x(:,ii,tt)'])*theta_i(:,ii)+normrnd(0,1);    % N by TT
     end
 end
-%for tt=2:TT
-% for ii=1:N
-% x(ii,tt)=0.5*(x(ii,tt-1))+normrnd(0,1);     % N by TT
-% y(ii,tt)= phi_i(1,ii)*y(ii,tt-1)+beta_i(1,ii)*x(ii,tt) +normrnd(0,1);    % N by TT end
-%end
-%end
+
 
 y_NT=y(:,TT-T0:TT); % dicard first 50 time series N by T0+1 
-y_NT1=y_NT(:,1:T0);
-x_NT=x(:,TT-T0+1:TT); %  N by T0
-W_it=zeros(N,T0,1+k);  % N by T0 by 1+k 
+y_NT1=y_NT(:,1:T0);  % y_(i,-1); N by T0
+y_NT2=y_NT(:,2:T0+1);  % y_(i,T)  ; N by T0
 
-W_it(:,:,1)=y_NT1;  %  y_{i,-1}; N by T0 
-W_it(:,:,2)=x_NT; %  N by T0
+x_NT=x(:,:,TT-T0:TT); %  dicard first 50 time series for x ; k by N by T0+1
 
 
-theta_LS=zeros(1+k,N);
-W=zeros(1+k,T0,N);
+
+x_NT1=zeros(N, T0);
+x_NT2=zeros(N, T0);
+for it=1:T0
+x_NT1(:,it)=x_NT(1,:,it+1);   % x_(i,-1) ; N by T0 
+x_NT2(:,it)=x_NT(2,:,it+1);   % x_(i, T); N by T0
+end
+
+W_it=zeros(N,T0,1+k);  % N by T0 by 1+k  
+W_it(:,:,1)=y_NT1;  %  y_{i,-1}; N by T0   
+W_it(:,:,2)=x_NT1; %  x_(i,T); N by T0  
+W_it(:,:,3)=x_NT2; %  x_(i,T); N by T0  
+ 
+
+theta_LS=zeros(1+k,N); 
+W=zeros(T0,1+k,N);
 for iii=1:N
-W(:,:,iii)=[W_it(iii,:,1);W_it(iii,:,2)]; % 1+k by TT
-theta_LS(:,iii)= ((W(:,:,iii)*W(:,:,iii)')^(-1))*(W(:,:,iii)*y_NT(iii,2:T0+1)');
+W(:,:,iii)=[W_it(iii,:,1)', W_it(iii,:,2)',W_it(iii,:,3)']; % T0 by 1+k
+theta_LS(:,iii)= ((W(:,:,iii)'*W(:,:,iii))^(-1))*(W(:,:,iii)'*y_NT2(iii,:)');
 end
 LS_MG(:,sml)=nanmean(theta_LS,2); % LS_MG
 
-%sml=sml+1;
+sml=sml+1;
 end
 
 mean_phi= nanmean(LS_MG(1,:));
-mean_beta= nanmean(LS_MG(2,:));
+mean_beta1= nanmean(LS_MG(2,:));
+mean_beta2= nanmean(LS_MG(2,:));
 bias_mean_phi(idx_T, idx_N, idx_phi) = mean_phi - phi;
-bias_mean_beta(idx_T, idx_N,size(b,2) )=mean_beta - b;
+bias_mean_beta1(idx_T, idx_N )=mean_beta1 - b1;
+bias_mean_beta2(idx_T, idx_N )=mean_beta2 - b2;
+
 
 std_phi(idx_T, idx_N, idx_phi) = nanstd(LS_MG(1,:));
-std_phi(idx_T, idx_N, size(b,2)) = nanstd(LS_MG(2,:));
+std_beta1(idx_T, idx_N) = nanstd(LS_MG(2,:));
+std_beta2(idx_T, idx_N) = nanstd(LS_MG(3,:));
+
+
 rmse_phi(idx_T, idx_N,idx_phi) = sqrt( nanmean( (LS_MG(1,:)-phi).^2) ); 
-rmse_beta(idx_T, idx_N) = sqrt( nanmean( (LS_MG(2,:)-b).^2) ); 
+rmse_beta1(idx_T, idx_N) = sqrt( nanmean( (LS_MG(2,:)-b1).^2) ); 
+rmse_beta2(idx_T, idx_N) = sqrt( nanmean( (LS_MG(3,:)-b1).^2) ); 
 
 end
 end
 end
-filename = 'LS_MG4.mat';
+filename = 'LSMG.mat';
 save(filename)
 
